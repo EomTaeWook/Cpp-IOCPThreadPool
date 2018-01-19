f1e354ee358f104a9a6eb23c8dd990e631c41ea3
EomTaeWook <trim0917@gmail.com>
2018-01-19 15:04:41 +0900

    Update Finally.h

--- ThreadPoolLib/Finally.h
+++ ThreadPoolLib/Finally.h
@@ -2,7 +2,7 @@
 #include<functional>
 //작성자 엄태욱
 //C# Java 언어의 try catch finally 처럼 실행하기 위해
-//함수를 벗어날때 소멸자를 사용하는 스마트 포인터로 Finall 구문 실행
+//함수를 벗어날때 소멸자를 사용하는 스마트 포인터로 Finally 구문 실행
 class Finally
 {
 private:
