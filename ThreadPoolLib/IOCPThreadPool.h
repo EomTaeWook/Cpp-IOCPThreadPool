56d63d0d383a3cffe9056a0acc9448b96aaf9224
eomtaewook <trim0917@gmail.com>
2018-01-22 09:44:57 +0900

    Stop Remove

--- ThreadPoolLib/IOCPThreadPool.h
+++ ThreadPoolLib/IOCPThreadPool.h
@@ -25,9 +25,10 @@ public:
 		DeleteCriticalSection(&cs);
 	}
 	bool Init(unsigned int threadSize = 0);
-	bool Stop();
+	
 	bool InsertQueueItem(WaitCallback waitCallback, void* args);
 private:
+	bool Stop();
 	bool DeleteItem(CWaitCallback* WaitCallback);
 	int Run();
 	static unsigned int __stdcall WorkerThread(void*);
